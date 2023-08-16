b5ebd6cd5adcb5e6b289deea85eb276f32be9c65
timchristeyreid <timothy84@hotmail.co.uk>
2023-08-16 15:32:26 +0100

    WIP - Adds tests for new DB connection

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,25 @@
 from starlette.testclient import TestClient
 
+import json
+
 from app.main import app
 
 client = TestClient(app)
 
 def test_create_beer():
     ''' Tests the create_beer function with a valid input'''
-    response = client.post(
-        "/create-beer/",
-        json={
-            "id": 2, 
+    test_request_payload = {
+            "id": 4, 
             "name": "Lager", 
-            "abv": 4}
+            "abv": 4
+            }
+    response = client.post(
+        "/create-beer/", content=
+        json.dumps(test_request_payload)
     )
     assert response.status_code == 200
     assert response.json() == {
-        "id": 2, 
+        "id": 4, 
         "name": "Lager", 
         "abv": 4}
 
@@ -24,20 +28,20 @@ def test_get_beer_by_id():
     assert response.status_code == 200
     assert response.json() == {
             "id":1,
-            "name":"IPA",
-            "abv":5
+            "name":"string",
+            "abv":0
         }
 
-def test_create_invalid_existing_beer():
-    response = client.post(
-        "/create-beer/",
-        json={
-            "id": 1, 
-            "name": "IPA", 
-            "abv": 5}
-    )
-    assert response.status_code == 400
-    assert response.json() == {"detail":"Beer ID already exists"}
+#def test_create_invalid_existing_beer():
+    #response = client.post(
+        #"/create-beer/",
+        #json={
+            #"id": 1, 
+            #"name": "IPA", 
+            #"abv": 5}
+    #)
+    #assert response.status_code == 400
+    #assert response.json() == {"detail":"Beer ID already exists"}
 
 def test_get_beer_invalid_id():
     response = client.get("/beer/10")
